aef3301ec36fc6f2d1479619d5b802f16d947d30
Rodrigo Hulle <rmhulle@gmail.com>
2016-05-23 18:20:25 -0300

    adicionando tradução pt-BR

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -12,12 +12,12 @@ class Invoice
 # só quem pode emitir uma nota é um Fornecedor, e está será sempre vinculada
 # a um contrato. Normalmente um contrato é executado por meio de várias notas
 # Caso um contrato seja deletado, assim como fornecedor suas notas também serão,
-#pois não haverá a quem vuncular e não será permitido notas soltas.
+# pois não haverá a quem vuncular e não será permitido notas soltas.
 
   belongs_to :vendor, dependent: :destroy
   belongs_to :contract, dependent: :destroy
 
-#TODO binding contrato e já vincular automaticamente ao fornecedor.
+# TODO binding contrato e já vincular automaticamente ao fornecedor.
 # Sem que o usuário faça. Por enquanto esta manual.
 # Tem que entender como funciona as referencias em mongo
 
